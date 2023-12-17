33fd3599bbb5278a48c9f35fbe2b899215fc5f94
jal1077 <jal1077@nyu.edu>
2023-12-16 22:17:19 -0500

    updated the sql schema and data

--- roulette_ticket/02-broadway_data.sql
+++
@@ -1,8 +0,0 @@
-use broadwayshow; 
-
-insert into broadway (id, show_name, show_time, show_date) values 
- 	(1, 'Wicked', '4:00 PM', 'October 14, 2025'),
- 	(2,'Moulin Rouge! The Musical', '5:00 PM', 'October 20, 2025'),
- 	(3,'Chicago', '6:00 PM', 'October 10, 2025'),
- 	(4,'The Lion King', '7:00 PM', 'October 7, 2025'),
- 	(5,'The Notebook', '4:00 PM', 'October 18, 2025');
